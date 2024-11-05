50dc519c619cc9a8bbad86812997b8500911f8c0
ufaoo <ufa@gmail.com>
2024-11-05 16:00:46 +0800

    msg log

--- response/respone.go
+++ response/respone.go
@@ -27,3 +27,23 @@ func Response(w http.ResponseWriter, data interface{}, err error) {
 	}
 	httpx.WriteJson(w, http.StatusBadRequest, body)
 }
+
+type code int
+
+const (
+	ResCode        code = iota + 199
+	ResSuccessCode      // 前端200的自定义状态码 不再渲染，
+)
+const (
+	MsgSuccess = "Success"
+)
+
+func NewResponse(flag bool, msg string, data interface{}, code int) (resp *Body) {
+	resp = &Body{
+		Data: data,
+		Flag: flag,
+		Msg:  msg,
+		Code: code,
+	}
+	return
+}
